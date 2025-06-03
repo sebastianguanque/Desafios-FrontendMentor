4e14bc22fe655c96e46fe8dec5cb42296aef5df3
Sebastián Guanque <guanquesebastian@gmail.com>
2025-06-03 16:27:52 -0300

    fix: Corregir visualización en dispositivos móviles y mejoras  de códigos

--- JS/article preview component master/js/app.js
+++ JS/article preview component master/js/app.js
@@ -1,7 +1,6 @@
-const button = document.getElementById('button')
-const icons = document.getElementById('icons')
+const button = document.getElementById("button");
+const icons = document.getElementById("icons");
 
-button.addEventListener('click', () => {
-  icons.classList.toggle('change')
-})
-
+button.addEventListener("click", () => {
+  icons.classList.toggle("change");
+});
--- JS/ping coming soon page master/js/app.js
+++ JS/ping coming soon page master/js/app.js
@@ -1,21 +1,21 @@
-const inputEmail = document.getElementById('email');
-const button = document.getElementById('button');
-const error = document.getElementById('error');
+const inputEmail = document.getElementById("email");
+const button = document.getElementById("button");
+const error = document.getElementById("error");
 
-button.addEventListener('click', ()=>{
+button.addEventListener("click", () => {
   validarEmail(inputEmail.value);
 });
 
 function validarEmail(email) {
-  let regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+  let regExp =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
 
-  if(regExp.test(email)) {
-    inputEmail.style.border = '1px solid hsl(223, 87%, 63%)';
-    error.style.visibility = 'hidden';
-    inputEmail.value = '';
+  if (regExp.test(email)) {
+    inputEmail.style.border = "1px solid hsl(223, 87%, 63%)";
+    error.style.visibility = "hidden";
+    inputEmail.value = "";
   } else {
-    inputEmail.style.border = '1px solid red';
-    error.style.visibility = 'visible';
-
+    inputEmail.style.border = "1px solid red";
+    error.style.visibility = "visible";
   }
 }
